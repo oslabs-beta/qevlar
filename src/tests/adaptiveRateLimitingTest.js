546401c6ff59f4c9f538850e840deeb3c1ab9df6
Landon Osteen <landonwyatteosteen@gmail.com>
2024-01-06 12:39:02 -0500

    configuration file validation

--- src/tests/adaptiveRateLimitingTest.js
+++ src/tests/adaptiveRateLimitingTest.js
@@ -1,15 +1,20 @@
-const config = require('../qevlarConfig.json');
-const { greenBold, highlight, green } = require('../../color');
+const config = require("../qevlarConfig.json");
+const { greenBold, highlight, green } = require("../../color");
+const configValidation = require("../tests/configValidation");
 
 async function adaptiveRateLimitingTest(returnToTestMenu) {
+  configValidation(config);
+
   const query = `{ ${config.TOP_LEVEL_FIELD} { ${config.SUB_FIELD} } }`;
   let rate = config.INITIAL_RATE;
   let success = true;
 
-  console.log('Starting Adaptive Rate Limiting Test...');
+  console.log("Starting Adaptive Rate Limiting Test...");
 
   while (success && rate < config.QUERY_RATE_LIMIT) {
-    console.log(green('Testing at rate: ') + `${rate} requests per unit time...`);
+    console.log(
+      green("Testing at rate: ") + `${rate} requests per unit time...`
+    );
 
     try {
       for (let i = 0; i < rate; i++) {
@@ -19,18 +24,26 @@ async function adaptiveRateLimitingTest(returnToTestMenu) {
       rate += config.INCREMENT;
     } catch (error) {
       success = false;
-      console.log(greenBold('\nTest completed\n'));
-      console.log(highlight('Summary of Test Failure:'));
+      console.log(greenBold("\nTest completed\n"));
+      console.log(highlight("Summary of Test Failure:"));
       console.log(`- Failed at rate: ${rate} requests per unit time.`);
       console.log(`- Error Message: ${error.message}`);
-      console.log(`- Possible rate limit of the API is just below ${rate} requests per unit time.\n`);
+      console.log(
+        `- Possible rate limit of the API is just below ${rate} requests per unit time.\n`
+      );
     }
   }
 
   if (!success) {
-    console.log('Consider adjusting the rate limits for better performance or resilience.');
+    console.log(
+      "Consider adjusting the rate limits for better performance or resilience."
+    );
   } else {
-    console.log(greenBold('Test concluded: No rate limiting detected within the tested range.'));
+    console.log(
+      greenBold(
+        "Test concluded: No rate limiting detected within the tested range."
+      )
+    );
   }
 
   if (returnToTestMenu) returnToTestMenu();
@@ -38,12 +51,12 @@ async function adaptiveRateLimitingTest(returnToTestMenu) {
 
 async function sendGraphQLRequest(url, query) {
   const response = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
-    body: JSON.stringify({ query })
+    body: JSON.stringify({ query }),
   });
 
   if (!response.ok) {
