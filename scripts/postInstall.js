0de23d53856bf6312bfcea7d8812ad24c26eacd1
John <johnnoh0719@gmail.com>
2024-01-09 16:49:06 -0600

    Ready to publish

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const scriptName = 'test';
-const scriptCommand = 'node node_modules/qevlar/scripts/qevlar';
+const scriptName = 'qevlar';
+const scriptCommand = 'node node_modules/qevlar';
 
-const packageJsonPath = path.join(process.cwd(), 'package.json');
+const packageJsonPath = path.join(__dirname, 'package.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
 
 packageJson.scripts[scriptName] = scriptCommand;
