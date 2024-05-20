c191df7b7312b37a9d41430c43e628a7d914ebb5
conorbell <conorbell27@gmail.com>
2024-05-20 11:55:40 -0400

    field dupe jest tests added still need to successfully match console logs

--- src/tests/fieldDuplicationTest.js
+++ src/tests/fieldDuplicationTest.js
@@ -1,6 +1,6 @@
-const { greenBold, redBold, yellowBold, highlight } = require("../../color");
-const config = require("../../qevlarConfig.json");
-const validateConfig = require("../../__tests__/validateConfig");
+const { greenBold, redBold, yellowBold, highlight } = require('../../color');
+const config = require('../../qevlarConfig.json');
+const validateConfig = require('../../__tests__/validateConfig');
 
 // Tests vulnerability to field duplication
 async function fieldDuplicationTest(returnToTestMenu) {
@@ -8,26 +8,29 @@ async function fieldDuplicationTest(returnToTestMenu) {
 
   try {
     const response = await fetch(config.API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ query }),
     });
 
-    if (!response.ok) throw new Error("Network response was not ok.");
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
 
     const result = await response.json();
+
     const stringResult = JSON.stringify(result);
-    console.log(redBold("\nTest failed:"));
-    console.log(highlight("API accepted duplicate fields.\n"));
-    console.log(yellowBold("API returned:"), `\n${stringResult}\n`);
+    console.log(redBold('\nTest failed:'));
+    console.log(highlight('API accepted duplicate fields.\n'));
+    console.log(yellowBold('API returned:'), `\n${stringResult}\n`);
   } catch (error) {
-    console.log(greenBold("\nTest passed:"));
-    console.log(highlight("API rejected duplicate fields.\n"));
-    console.log("\nSummary of Error");
-    console.log("Error: " + error.message);
+    console.log(greenBold('\nTest passed:'));
+    console.log(highlight('API rejected duplicate fields.\n'));
+    console.log('\nSummary of Error');
+    console.log('Error: ' + error.message);
   }
 
-  if (returnToTestMenu && typeof returnToTestMenu === "function")
+  if (returnToTestMenu && typeof returnToTestMenu === 'function')
     returnToTestMenu();
 }
 
